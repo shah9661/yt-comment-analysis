629601e4af58e7f38863d9d69df90e1b20837539
shah9661 <shanshadalam96@gmail.com>
2026-09-02 02:07:07 +0530

    cross middleware added

--- fastapi_app/app.py
+++ fastapi_app/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi_app.model_loader import load_production_resources
 from fastapi_app.clean_comment import preprocess_comment
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class PredictionRequest(BaseModel):
     comment: str
 
